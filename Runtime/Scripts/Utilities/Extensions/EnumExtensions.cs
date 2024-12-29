1dd919592106e1c441f999bb8d7e60365c833ea5
Alex Bedard-Reid <alexbedardreid@gmail.com>
2024-12-29 14:38:15 -0800

    Implement Enum Extension for selecting random value

---
+++ Runtime/Scripts/Utilities/Extensions/EnumExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Utilities
+{
+    public static class EnumExtensions
+    {
+        private static Dictionary<Type, Enum[]> _enumsMap;
+        
+        public static T GetRandomEnum<T>() where T : Enum
+        {
+            var type = typeof(T);
+            
+            if (_enumsMap == null)
+            {
+                _enumsMap = new Dictionary<Type, Enum[]>();
+            }
+
+            if (_enumsMap.TryGetValue(type, out var enums) == false)
+            {
+                enums = (Enum[])Enum.GetValues(type);
+                _enumsMap.Add(type, enums);
+            }
+
+            return (T)enums.PickRandomElement();
+        }
+    }
+}
